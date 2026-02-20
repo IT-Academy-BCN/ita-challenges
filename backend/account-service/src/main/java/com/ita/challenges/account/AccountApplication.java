2f3cb2afae07b9e14a3655ef7c33b81a5f037542
Ivan Vilaro <vilaro.ivan@gmail.com>
2026-02-20 14:19:40 +0100

    refactor(account-service): rename base package and main class to account

--- backend/account-service/src/main/java/com/ita/challenges/account/AccountApplication.java
+++ backend/account-service/src/main/java/com/ita/challenges/account/AccountApplication.java
@@ -1,13 +1,13 @@
-package com.ita.challenges.user;
+package com.ita.challenges.account;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class UserApplication {
+public class AccountApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(UserApplication.class, args);
+		SpringApplication.run(AccountApplication.class, args);
 	}
 
 }
