96b7b0e2f5902001238b3b107a25988803d98a2e
eariasvalor <eariasvalor@gmail.com>
2026-04-24 00:40:56 +0200

    refactor: remove validations for being allegedly out of scope

--- backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/in/dto/ChallengeRequest.java
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/in/dto/ChallengeRequest.java
@@ -1,13 +1,3 @@
 package com.itachallenges.challengeservice.catalog.infrastructure.adapter.web.in.dto;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Size;
-
-public record ChallengeRequest(
-        @NotBlank
-        @Size (min = 3, max = 100)
-        String title,
-
-        @NotBlank
-        @Size (min = 3,max = 1000)
-        String description) {}
+public record ChallengeRequest(String title, String description) {}
