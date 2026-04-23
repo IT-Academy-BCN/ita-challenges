96d8d1c348cb68d789a3105bf9a9115896f47f9c
eariasvalor <eariasvalor@gmail.com>
2026-04-23 13:53:12 +0200

    feat: add ChallengeRepository and InMemoryChallengeRepository for persistence managing

--- backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/out/persistence/InMemoryChallengeRepository.java
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/out/persistence/InMemoryChallengeRepository.java
@@ -1,11 +1,8 @@
 package com.itachallenges.challengeservice.catalog.infrastructure.adapter.web.out.persistence;
 
-import com.itachallenges.challengeservice.catalog.domain.model.Challenge;
 import com.itachallenges.challengeservice.catalog.domain.port.out.ChallengeRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
-import java.util.Map;
 
 @Repository
 public class InMemoryChallengeRepository implements ChallengeRepository {
