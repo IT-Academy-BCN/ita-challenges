1162d38d15b0960c05ea16a2ec4c48848e928da8
eariasvalor <eariasvalor@gmail.com>
2026-04-23 13:21:42 +0200

    refactor: add NotBlank and Size validations to ChallengeRequest

--- backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/in/dto/ChallengeRequest.java
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/in/dto/ChallengeRequest.java
@@ -1,3 +1,13 @@
 package com.itachallenges.challengeservice.catalog.infrastructure.adapter.web.in.dto;
 
-public record ChallengeRequest(String title, String description) {}
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+
+public record ChallengeRequest(
+        @NotBlank
+        @Size (min = 3, max = 100)
+        String title,
+
+        @NotBlank
+        @Size (min = 3,max = 1000)
+        String description) {}
