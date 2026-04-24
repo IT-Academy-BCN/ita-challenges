a3f3495e4d3684cd972fbee9a619666eb50e5e30
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-04-25 01:56:57 +0200

    fix(auth):After successful login, backend retrieves GitHub user identity

--- backend/account-service/src/main/java/com/ita/challenges/account/controller/HomeController.java
+++ backend/account-service/src/main/java/com/ita/challenges/account/controller/HomeController.java
@@ -1,13 +1,21 @@
 package com.ita.challenges.account.controller;
 
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
+import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Map;
 
 @RestController
 
 public class HomeController {
     @GetMapping ("/api/account/success")
-    public String home(){
-        return "Home Successfully";
+    public Map<String, Object> success(@AuthenticationPrincipal OAuth2User principal){
+        return Map.of(
+                "username", principal.getAttribute("login"),
+                "avatarUrl",principal.getAttribute("avartar_url") !=null
+                ? principal.getAttribute("avartar_url"): ""
+        );
     }
 }
