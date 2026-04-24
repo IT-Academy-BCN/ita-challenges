ffe1fd01cbb919b7beff1002fed933d7c56ec1f8
Albert Medina <albertmecu@gmail.com>
2026-04-24 17:34:03 +0200

    feat: added empty implementation for create method in InMemoryChallengeRepository

--- backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/out/persistence/InMemoryChallengeRepository.java
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/out/persistence/InMemoryChallengeRepository.java
@@ -1,10 +1,14 @@
 package com.itachallenges.challengeservice.catalog.infrastructure.adapter.web.out.persistence;
 
+import com.itachallenges.challengeservice.catalog.domain.model.Challenge;
 import com.itachallenges.challengeservice.catalog.domain.port.out.ChallengeRepository;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public class InMemoryChallengeRepository implements ChallengeRepository {
-
+    @Override
+    public Challenge create(Challenge challenge) {
+        throw new UnsupportedOperationException("To be implemented");
+    }
 }
