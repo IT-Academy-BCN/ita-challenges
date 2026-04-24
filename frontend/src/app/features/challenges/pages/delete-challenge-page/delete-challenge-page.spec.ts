648b74ee7dc5612424fae8294c63ba7760d1c926
Juan Delgado <soy.juan.delgado@gmail.com>
2026-04-24 22:12:49 +0200

    feat: added tests

--- frontend/src/app/features/challenges/pages/delete-challenge-page/delete-challenge-page.spec.ts
+++ frontend/src/app/features/challenges/pages/delete-challenge-page/delete-challenge-page.spec.ts
@@ -1,14 +1,29 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { DeleteChallengePage } from './delete-challenge-page';
+import { ChallengeService } from '../../services/challenge.service';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
 
 describe('DeleteChallengePage', () => {
   let component: DeleteChallengePage;
   let fixture: ComponentFixture<DeleteChallengePage>;
+  let mockChallengeService: any;
+  let mockRouter: any;
 
   beforeEach(async () => {
+    mockChallengeService = {
+      delete: (id: string) => of({ success: true})
+    };
+    mockRouter = {
+      navigate: vi.fn()
+    };
     await TestBed.configureTestingModule({
-      imports: [DeleteChallengePage]
+      imports: [DeleteChallengePage],
+      providers: [
+        { provide: ChallengeService, useValue: mockChallengeService},
+        { provide: Router, useValue: mockRouter }
+      ]
     })
     .compileComponents();
 
@@ -20,4 +35,41 @@ describe('DeleteChallengePage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should start the form with empty field', () => {
+    const id = component.deleteChallengeForm.get('challengeId')
+    
+    expect(id?.value).toBe('')
+  })
+
+  it('should call onSubmit when form is submitted', () => {
+    vi.spyOn(component, 'onSubmit')
+
+    const form = fixture.nativeElement.querySelector('form')
+    form.dispatchEvent(new Event('submit'));
+    
+    expect(component.onSubmit).toHaveBeenCalled();
+  })
+
+  it('should call challengeService.delete when call OnSubmit with correct challengeId', () => {
+    const testId = '123'
+    component.deleteChallengeForm.setValue({challengeId: testId})
+
+    vi.spyOn(mockChallengeService, 'delete').mockReturnValue(of({success: true}))
+
+    component.onSubmit()
+    expect(mockChallengeService.delete).toHaveBeenCalledWith(testId);
+  })
+
+  it('should call goChallenges when call onSubmit', () => {
+    vi.spyOn(component, 'goChallenges')
+
+    component.onSubmit()
+    expect(component.goChallenges).toHaveBeenCalled();
+  })
+
+  it('should navigate when call goChallenges', () => {
+    component.goChallenges()
+    expect(mockRouter.navigate).toHaveBeenCalled();
+  })
 });
