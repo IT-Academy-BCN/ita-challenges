34995d223fc84a7c032c8af9f921adb1e1d27ce0
Juan Delgado <soy.juan.delgado@gmail.com>
2026-04-24 20:22:15 +0200

    feat: created form and test submit function

--- frontend/src/app/features/challenges/pages/delete-challenge-page/delete-challenge-page.ts
+++ frontend/src/app/features/challenges/pages/delete-challenge-page/delete-challenge-page.ts
@@ -1,11 +1,22 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-delete-challenge-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './delete-challenge-page.html',
   styleUrl: './delete-challenge-page.css',
 })
 export class DeleteChallengePage {
 
+  readonly fb = inject(FormBuilder);
+
+  deleteChallengeForm = this.fb.group({
+    challengeId: ['']
+  })
+
+  onSubmit(){
+    const challengeId = this.deleteChallengeForm.value.challengeId
+    console.log(challengeId)
+  }
 }
