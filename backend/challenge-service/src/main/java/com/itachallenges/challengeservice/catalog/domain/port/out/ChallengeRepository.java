4aa6fa2f3fa02c32a077d4613e3b80a4fdbb2f4e
Andres <andrescalvo01@gmail.com>
2026-04-25 11:58:09 +0200

    [Update] Define updateChallenge method in domain output port
    # Conflicts:
    #	backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/domain/port/out/ChallengeRepository.java
    #	backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/out/persistence/InMemoryChallengeRepository.java

--- backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/domain/port/out/ChallengeRepository.java
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/domain/port/out/ChallengeRepository.java
@@ -1,5 +1,9 @@
 package com.itachallenges.challengeservice.catalog.domain.port.out;
 
+import com.itachallenges.challengeservice.catalog.domain.model.Challenge;
+
 public interface ChallengeRepository {
+
+    Challenge update(Challenge challenge);
     void delete(String id);
 }
