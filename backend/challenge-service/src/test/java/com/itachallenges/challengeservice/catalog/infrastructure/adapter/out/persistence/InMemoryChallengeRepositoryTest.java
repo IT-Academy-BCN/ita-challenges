b136728b0ef51fa57673ce3be0908c2a4d60a904
eariasvalor <eariasvalor@gmail.com>
2026-04-25 15:16:45 +0200

    test: create InMemoryChallengeRepositoryTest file with testing of empty storage

---
+++ backend/challenge-service/src/test/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/out/persistence/InMemoryChallengeRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.itachallenges.challengeservice.catalog.infrastructure.adapter.out.persistence;
+
+import com.itachallenges.challengeservice.catalog.domain.model.Challenge;
+import com.itachallenges.challengeservice.catalog.infrastructure.adapter.web.out.persistence.InMemoryChallengeRepository;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InMemoryChallengeRepositoryTest {
+
+    private final InMemoryChallengeRepository repository = new InMemoryChallengeRepository();
+
+    @Test
+    void should_return_empty_list_when_no_challenges_exist() {
+        List<Challenge> result = repository.findAll();
+
+        assertThat(result).isEmpty();
+    }
+}
