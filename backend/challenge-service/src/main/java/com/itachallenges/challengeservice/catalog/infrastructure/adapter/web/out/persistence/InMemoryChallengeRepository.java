dbe470f25d34d745f0fe445bacc0eebd07504e19
Andres <andrescalvo01@gmail.com>
2026-04-25 17:11:01 +0200

    [Update] Implement update method in infrastructure adapter

--- backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/out/persistence/InMemoryChallengeRepository.java
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/out/persistence/InMemoryChallengeRepository.java
@@ -2,17 +2,29 @@ package com.itachallenges.challengeservice.catalog.infrastructure.adapter.web.ou
 
 import com.itachallenges.challengeservice.catalog.domain.model.Challenge;
 import com.itachallenges.challengeservice.catalog.domain.port.out.ChallengeRepository;
+import com.itachallenges.challengeservice.catalog.domain.valueobject.ChallengeId;
 import org.springframework.stereotype.Repository;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 
 @Repository
 public class InMemoryChallengeRepository implements ChallengeRepository {
 
+    Map<ChallengeId, Challenge> storage = new ConcurrentHashMap<>();
+
     @Override
     public Challenge update(Challenge challenge) {
-        throw new UnsupportedOperationException("To be implemented");
-    }
 
+        ChallengeId id = challenge.getId();
+
+        if (!storage.containsKey(id)) {
+            throw new RuntimeException("Challenge not found with id: " + id);
+        }
+
+        storage.put(id, challenge);
+        return challenge;
+    }
     @Override
     public void delete(String id) {
         throw new UnsupportedOperationException("Not implemented yet");
