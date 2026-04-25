238da27d5afff5147017c7887c5e4fba74010e00
Andres <andrescalvo01@gmail.com>
2026-04-25 12:26:20 +0200

    chore: changing name to updateChallenge to just update

--- backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/domain/port/out/ChallengeRepository.java
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/domain/port/out/ChallengeRepository.java
@@ -5,5 +5,6 @@ import com.itachallenges.challengeservice.catalog.domain.model.Challenge;
 public interface ChallengeRepository {
 
     Challenge update(Challenge challenge);
+
     void delete(String id);
 }
