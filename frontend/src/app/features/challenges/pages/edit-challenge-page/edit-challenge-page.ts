4acf3357c0decf870769e6a325adb5829500f2d5
nereame96 <nereame96@gmail.com>
2026-04-26 20:08:20 +0200

    feat(challenge): implement reactive form with FormBuilder and basic HTML (#325)

--- frontend/src/app/features/challenges/pages/edit-challenge-page/edit-challenge-page.ts
+++ frontend/src/app/features/challenges/pages/edit-challenge-page/edit-challenge-page.ts
@@ -1,11 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { ChallengeService } from '../../services/challenge.service';
+import { Router } from '@angular/router';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { IChallengeRequest } from '../../models/ichallenge-request.interface';
+
 
 @Component({
   selector: 'app-edit-challenge-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './edit-challenge-page.html',
   styleUrl: './edit-challenge-page.css',
 })
 export class EditChallengePage {
+
+  readonly challengeService = inject(ChallengeService)
+  readonly router = inject(Router)
+  readonly fb = inject(FormBuilder)
+
+  editForm = this.fb.group({
+    id: [''],
+    title: [''],
+    description: ['']
+  })
+
+  onSubmit() {
+
+    const { id, title, description} = this.editForm.getRawValue()
+
+    const challengePayload: IChallengeRequest = {
+      title: title ?? '',
+      description: description ?? ''
+    }
+
+    this.challengeService.update(id?? '', challengePayload).subscribe({
+      next: () => {
+        this.goChallenges()
+      },
+    })
+  }
+
+  goChallenges() {
+    this.router.navigate(['/challenges']);
+  }
 
 }
