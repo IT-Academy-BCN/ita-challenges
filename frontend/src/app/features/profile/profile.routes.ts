cb50a4c4c332eacd8869f4a93f3514035e0faf34
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-04-26 16:57:33 +0200

    fix(profile): fix routing to point to correct profile page component

--- frontend/src/app/features/profile/profile.routes.ts
+++ frontend/src/app/features/profile/profile.routes.ts
@@ -1,11 +1,11 @@
 import { Routes } from '@angular/router';
-import { ProfilePage } from './pages/profile-page.component';
+import { ProfilePageComponent } from './pages/profile-page/profile-page';
 import { authGuard } from '../../core/guards/auth-guard';
 
 export const PROFILE_ROUTES: Routes = [
   {
     path: '',
-    component: ProfilePage,
+    component: ProfilePageComponent,
     canActivate: [authGuard],
   },
 ];
