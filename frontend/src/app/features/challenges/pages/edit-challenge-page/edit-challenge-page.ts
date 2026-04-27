42cb160c7d0686b80d713ed3f2ebf52b81e1336b
nereame96 <nereame96@gmail.com>
2026-04-27 10:24:48 +0200

    fix(challenge): remove id disabled true (#325)

--- frontend/src/app/features/challenges/pages/edit-challenge-page/edit-challenge-page.ts
+++ frontend/src/app/features/challenges/pages/edit-challenge-page/edit-challenge-page.ts
@@ -18,7 +18,7 @@ export class EditChallengePage {
   private readonly fb = inject(FormBuilder);
 
   editForm = this.fb.group({
-    id: [{value: '', disabled: true}],
+    id: [''],
     title: [''],
     description: ['']
   });
