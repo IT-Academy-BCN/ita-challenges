6d56bed3ae26eb8084bebf396ab74a96e38bf371
eariasvalor <eariasvalor@gmail.com>
2026-04-27 14:26:46 +0200

    feat: implement save method in repository and its adapter

--- backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/out/persistence/InMemoryChallengeRepository.java
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/out/persistence/InMemoryChallengeRepository.java
@@ -1,10 +1,27 @@
 package com.itachallenges.challengeservice.catalog.infrastructure.adapter.web.out.persistence;
 
+import com.itachallenges.challengeservice.catalog.domain.model.Challenge;
 import com.itachallenges.challengeservice.catalog.domain.port.out.ChallengeRepository;
+import com.itachallenges.challengeservice.catalog.domain.valueobject.ChallengeId;
 import org.springframework.stereotype.Repository;
 
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class InMemoryChallengeRepository implements ChallengeRepository {
 
+    private final Map<ChallengeId, Challenge> storage = new ConcurrentHashMap<>();
+
+    @Override
+    public List<Challenge> findAll() {
+        return new ArrayList<>(storage.values());
+    }
+
+    @Override
+    public void save(Challenge challenge) {
+        storage.put(challenge.getId(), challenge);
+    }
 }
