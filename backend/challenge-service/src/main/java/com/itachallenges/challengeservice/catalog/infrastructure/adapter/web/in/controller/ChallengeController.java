d2878d7a9b832dfe22b75f182eaf94711ef64d91
eariasvalor <eariasvalor@gmail.com>
2026-04-27 14:27:09 +0200

    feat: implement post endpoint to create a challenge in ChallengeController

--- backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/in/controller/ChallengeController.java
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/in/controller/ChallengeController.java
@@ -1,19 +1,25 @@
 package com.itachallenges.challengeservice.catalog.infrastructure.adapter.web.in.controller;
 
 import com.itachallenges.challengeservice.catalog.domain.model.Challenge;
+import com.itachallenges.challengeservice.catalog.domain.port.out.ChallengeRepository;
 import com.itachallenges.challengeservice.catalog.infrastructure.adapter.web.in.dto.ChallengeResponse;
 import com.itachallenges.challengeservice.catalog.infrastructure.adapter.web.in.dto.ChallengeRequest;
+import lombok.AllArgsConstructor;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
+@AllArgsConstructor
 @RequestMapping("/api/challenge")
 public class ChallengeController {
+
+    private final ChallengeRepository challengeRepository;
 
     @PostMapping
     public ResponseEntity<ChallengeResponse> create(@RequestBody ChallengeRequest request) {
         Challenge challenge = Challenge.create(request.title(), request.description());
+        challengeRepository.save(challenge);
 
         return ResponseEntity.status(HttpStatus.CREATED).body(
                 new ChallengeResponse(
