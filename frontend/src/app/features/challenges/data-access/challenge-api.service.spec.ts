9c29d25546b74f1596233d15617cfaa1b27d715f
Juan Delgado <soy.juan.delgado@gmail.com>
2026-04-27 17:05:22 +0200

    feat: added API connection and tests

--- frontend/src/app/features/challenges/data-access/challenge-api.service.spec.ts
+++ frontend/src/app/features/challenges/data-access/challenge-api.service.spec.ts
@@ -1,16 +1,51 @@
 import { TestBed } from '@angular/core/testing';
+import { HttpTestingController, provideHttpClientTesting } from '@angular/common/http/testing';
+import { provideHttpClient } from '@angular/common/http';
 
 import { ChallengeApiService } from './challenge-api.service';
+import { CHALLENGES_MOCK } from '../models/challenges.mock';
 
 describe('ChallengeApiService', () => {
   let service: ChallengeApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      providers: [
+        provideHttpClient(),
+        provideHttpClientTesting(),
+      ]
+    });
     service = TestBed.inject(ChallengeApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should load all challenges via GET', () => {
+    service.loadAll().subscribe((challenges) => {
+      expect(challenges).toEqual(CHALLENGES_MOCK);
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:8080/api/challenge/');
+    expect(req.request.method).toBe('GET');
+    
+    req.flush(CHALLENGES_MOCK); 
+  });
+
+  it('should return mocked challenges on HTTP error', () => {
+    service.loadAll().subscribe((challenges) => {
+      expect(challenges).toEqual(CHALLENGES_MOCK);
+    });
+
+    const req = httpTestingController.expectOne('http://localhost:8080/api/challenge/');
+    
+    req.flush('Error interno', { status: 500, statusText: 'Internal Server Error' });
   });
 });
