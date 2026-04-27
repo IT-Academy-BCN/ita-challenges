82b5f0d5e3cc88fd0bdd071fbb826d933874348f
Albert Medina <albertmecu@gmail.com>
2026-04-27 12:08:19 +0200

    test: created InMemoryChallengeRepositoryTest with a test for creating new challenge

---
+++ backend/challenge-service/src/test/java/com/itachallenges/challengeservice/catalog/infrastructure/adapter/web/out/persistence/InMemoryChallengeRepositoryTest.java
@@ -0,0 +1,21 @@
+package com.itachallenges.challengeservice.catalog.infrastructure.adapter.web.out.persistence;
+
+import com.itachallenges.challengeservice.catalog.domain.model.Challenge;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+class InMemoryChallengeRepositoryTest {
+
+    private final InMemoryChallengeRepository repository = new InMemoryChallengeRepository();
+
+    @Test
+    void should_create_new_challenge() {
+        Challenge newChallenge = Challenge.create("New Challenge Title", "New Challenge Description");
+
+        Challenge result = repository.save(newChallenge);
+
+        assertThat(result.getTitle().toString()).isEqualTo("New Challenge Title");
+        assertThat(result.getDescription().toString()).isEqualTo("New Challenge Description");
+    }
+}
