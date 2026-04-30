6225922980b9fac20de948171244ef40f50b458e
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-04-30 08:56:22 +0200

    refactor(auth-guard): remove unused imports

--- frontend/src/app/core/guards/auth-guard.ts
+++ frontend/src/app/core/guards/auth-guard.ts
@@ -1,7 +1,6 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../features/auth/data-access/auth-service';
-import { catchError, map, of } from 'rxjs';
 
 export const authGuard: CanActivateFn = () => {
   const auth = inject(AuthService);
