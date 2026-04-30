32c1d921cd591ffade75f9b98a007ecc7220f592
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-04-30 08:10:29 +0200

    refactor(auth): replace if/else with ternary in auth guard for improved readability

--- frontend/src/app/core/guards/auth-guard.ts
+++ frontend/src/app/core/guards/auth-guard.ts
@@ -1,13 +1,15 @@
 import { inject } from '@angular/core';
 import { CanActivateFn, Router } from '@angular/router';
 import { AuthService } from '../../features/auth/data-access/auth-service';
+import { catchError, map, of } from 'rxjs';
 
 export const authGuard: CanActivateFn = () => {
   const auth = inject(AuthService);
   const router = inject(Router);
 
-  if (auth.getUser()) return true;
+  const user = auth.getUser();
 
-  router.navigate(['/auth']);
-  return false;
+  return user
+    ? true
+    : router.createUrlTree(['/auth']);
 };
