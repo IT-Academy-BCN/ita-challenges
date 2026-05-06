3a4fa3c37e39c281b7cd86d0bbf605f279f22f52
Albert Medina <albertmecu@gmail.com>
2026-05-06 14:42:29 +0200

    test: add UserControllerTest with a test for create endpoint

--- backend/account-service/src/test/java/com/ita/challenges/account/infrastructure/adapter/web/in/UserControllerTest.java
+++ backend/account-service/src/test/java/com/ita/challenges/account/infrastructure/adapter/web/in/UserControllerTest.java
@@ -1,4 +1,22 @@
 package com.ita.challenges.account.infrastructure.adapter.web.in;
 
-public class UserControllerTest {
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
+import org.springframework.test.web.servlet.MockMvc;
+
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
+
+@WebMvcTest(UserController.class)
+class UserControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @Test
+    void create_shouldReturn200() throws Exception {
+        mockMvc.perform(post("/api/account/users"))
+                .andExpect(status().isOk());
+    }
 }
