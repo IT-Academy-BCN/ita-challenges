6c1bc08ab7cab987de10ea958a10ea2cbda9361b
Albert Medina <albertmecu@gmail.com>
2026-05-06 15:03:43 +0200

    test: fix security issues in user creation test

--- backend/account-service/src/test/java/com/ita/challenges/account/infrastructure/adapter/web/in/UserControllerTest.java
+++ backend/account-service/src/test/java/com/ita/challenges/account/infrastructure/adapter/web/in/UserControllerTest.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.test.web.servlet.MockMvc;
 
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
+import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
@@ -16,7 +18,10 @@ class UserControllerTest {
 
     @Test
     void create_shouldReturn200() throws Exception {
-        mockMvc.perform(post("/api/account/users"))
+        mockMvc.perform(post("/api/account/users")
+                        .with(oauth2Login())
+                        .with(csrf())
+                )
                 .andExpect(status().isOk());
     }
 }
