456b43b87046f3fd3fca7797250ca8a21a9b007e
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-05-07 07:12:09 +0200

    style(challenges-list): normalize import formatting (#541)

--- frontend/src/app/features/challenges/pages/challenges-list-page/challenges-list-page.ts
+++ frontend/src/app/features/challenges/pages/challenges-list-page/challenges-list-page.ts
@@ -1,7 +1,7 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { IChallenge } from '../../models/ichallenge.interface';
 import { ChallengeService } from '../../services/challenge.service';
-import { CreateButtonComponent } from "../../components/buttons/create-button/create-button";
+import { CreateButtonComponent } from '../../components/buttons/create-button/create-button';
 
 @Component({
   selector: 'app-challenges-list-page',
