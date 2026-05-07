90008cc6c82cce9456b4426348d3f4810598b4e4
Juan Delgado <soy.juan.delgado@gmail.com>
2026-05-07 21:23:38 +0200

    fix: changed setUserRole return to observable

--- frontend/src/app/features/admin/data-access/admin-api.service.ts
+++ frontend/src/app/features/admin/data-access/admin-api.service.ts
@@ -1,13 +1,14 @@
 import { Injectable } from '@angular/core';
 import { Role } from '../../../core/models/role.enum';
+import { Observable, of } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
 })
 export class AdminApiService {
   
-  setUserRole(username: string, role: Role){
-    return
+  setUserRole(username: string, role: Role): Observable<void>{
+    return of(undefined)
   }
 
 }
