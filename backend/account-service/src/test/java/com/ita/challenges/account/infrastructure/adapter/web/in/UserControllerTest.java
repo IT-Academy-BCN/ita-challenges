f5d727dabb73a6e373dda2bece5a82a0420b2d6c
Albert Medina <albertmecu@gmail.com>
2026-05-08 18:06:32 +0200

    test: add test for user creation and update

--- backend/account-service/src/test/java/com/ita/challenges/account/infrastructure/adapter/web/in/UserControllerTest.java
+++ backend/account-service/src/test/java/com/ita/challenges/account/infrastructure/adapter/web/in/UserControllerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.databind.ObjectMapper;
 import com.ita.challenges.account.domain.model.Role;
 import com.ita.challenges.account.domain.model.User;
 import com.ita.challenges.account.domain.port.out.UserRepository;
+import com.ita.challenges.account.infrastructure.adapter.web.in.dto.GitHubUserResponse;
 import com.ita.challenges.account.infrastructure.adapter.web.in.dto.UserRequest;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,8 +12,11 @@ import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.web.client.RestTemplate;
 
-import static org.mockito.ArgumentMatchers.any;
+import java.util.Optional;
+
+import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.oauth2Login;
@@ -29,15 +33,24 @@ class UserControllerTest {
     @MockBean
     private UserRepository repository;
 
+    @MockBean
+    private RestTemplate restTemplate;
+
     @Autowired
     private ObjectMapper objectMapper;
 
     @Test
-    void create_shouldReturn201() throws Exception {
-        UserRequest request = new UserRequest("ID12345", Role.GUEST);
-        User saved = new User(request.username(), request.role());
+    void create_shouldReturn201WhenUserIsNew() throws Exception {
+        GitHubUserResponse gitHubUserResponse = new GitHubUserResponse("ID12345");
+        when(restTemplate.getForObject(anyString(), eq(GitHubUserResponse.class)))
+                .thenReturn(gitHubUserResponse);
+
+        when(repository.findByUsername("ID12345")).thenReturn(Optional.empty());
+
+        User saved = new User("ID12345", Role.GUEST);
         when(repository.save(any(User.class))).thenReturn(saved);
 
+        UserRequest request = new UserRequest("username12345", Role.GUEST);
         mockMvc.perform(post("/api/account/users")
                         .with(oauth2Login())
                         .with(csrf())
@@ -47,5 +60,28 @@ class UserControllerTest {
                 .andExpect(status().isCreated())
                 .andExpect(jsonPath("$.username").value("ID12345"))
                 .andExpect(jsonPath("$.role").value(Role.GUEST.name()));
+    }
+
+    @Test
+    void create_shouldReturn200AndUpdateUserWhenAlreadyExists() throws Exception {
+        GitHubUserResponse gitHubUserResponse = new GitHubUserResponse("ID12345");
+        when(restTemplate.getForObject(anyString(), eq(GitHubUserResponse.class)))
+                .thenReturn(gitHubUserResponse);
+
+        User existingUser = new User("ID12345", Role.GUEST);
+        when(repository.findByUsername("ID12345")).thenReturn(Optional.of(existingUser));
+
+        User saved = new User("ID12345", Role.STUDENT);
+        when(repository.save(any(User.class))).thenReturn(saved);
+
+        UserRequest request = new UserRequest("username12345", Role.STUDENT);
+        mockMvc.perform(post("/api/account/users")
+                        .with(oauth2Login()).with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+                .andExpect(status().isOk())
+                .andExpect(jsonPath("$.username").value("ID12345"))
+                .andExpect(jsonPath("$.role").value(Role.STUDENT.name()));
     }
 }
