17b0191d307d04388ab9a590d7b877ae34efc510
Spook242 <spook242@gmail.com>
2026-05-09 13:09:59 +0200

    feat(ticketing): define save method in TicketRepository

--- backend/account-service/src/main/java/com/ita/challenges/account/domain/port/out/TicketRepository.java
+++ backend/account-service/src/main/java/com/ita/challenges/account/domain/port/out/TicketRepository.java
@@ -1,4 +1,7 @@
 package com.ita.challenges.account.domain.port.out;
 
+import com.ita.challenges.account.domain.model.Ticket;
+
 public interface TicketRepository {
+    Ticket save(Ticket newTicket);
 }
--- backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/adapter/web/out/persistence/InMemoryTicketRepository.java
+++ backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/adapter/web/out/persistence/InMemoryTicketRepository.java
@@ -1,8 +1,13 @@
 package com.ita.challenges.account.infrastructure.adapter.web.out.persistence;
 
+import com.ita.challenges.account.domain.model.Ticket;
 import com.ita.challenges.account.domain.port.out.TicketRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
 public class InMemoryTicketRepository implements TicketRepository {
+    @Override
+    public Ticket save(Ticket newTicket) {
+        throw new UnsupportedOperationException("Save endpoint not implemented yet");
+    }
 }
