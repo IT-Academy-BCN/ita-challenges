b00f6f3c303fd7ba4b03a533893ae794fca3469c
Juan Delgado <soy.juan.delgado@gmail.com>
2026-05-11 17:44:28 +0200

    feat: created form, called TicketsApiService.create and navigate to /tickets

--- frontend/src/app/features/tickets/pages/create-ticket-page/create-ticket-page.spec.ts
+++ frontend/src/app/features/tickets/pages/create-ticket-page/create-ticket-page.spec.ts
@@ -1,14 +1,32 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { CreateTicketPage } from './create-ticket-page';
+import { TicketApiService } from '../../data-access/ticket-api.service';
+import { Router } from '@angular/router';
+import { of } from 'rxjs';
 
 describe('CreateTicketPage', () => {
   let component: CreateTicketPage;
   let fixture: ComponentFixture<CreateTicketPage>;
+  let mockTicketService: Partial<TicketApiService>;
+  let mockRouter: Partial<Router>;
 
   beforeEach(async () => {
+
+    mockTicketService = {
+      create: () => of({ id:'1', userId: '', title: '', description: '' })
+    };
+
+    mockRouter = {
+      navigate: vi.fn() as any
+    };
+
     await TestBed.configureTestingModule({
-      imports: [CreateTicketPage]
+      imports: [CreateTicketPage],
+      providers: [
+        { provide: TicketApiService, useValue: mockTicketService },
+        { provide: Router, useValue: mockRouter }
+      ]
     })
     .compileComponents();
 
@@ -20,4 +38,48 @@ describe('CreateTicketPage', () => {
   it('should create', () => {
     expect(component).toBeTruthy();
   });
+
+  it('should start the form with empty fields', () => {
+    const userId = component.ticketForm.get('userId');
+    const title = component.ticketForm.get('title');
+    const description = component.ticketForm.get('description');
+
+    expect(userId?.value).toBe('');
+    expect(title?.value).toBe('');
+    expect(description?.value).toBe('');
+  });
+
+  it('should call onSubmit when form is submitted', () => {
+    vi.spyOn(component, 'onSubmit');
+
+    const form = fixture.nativeElement.querySelector('form');
+    form.dispatchEvent(new Event('submit'));
+
+    expect(component.onSubmit).toHaveBeenCalled();
+  });
+
+  it('should call ticketApiService.create when call onSubmit with correct data', () => {
+    const testData = { userId: 'usuari', title: 'Nou Repte', description: 'Descripció' };
+    component.ticketForm.setValue(testData);
+
+    vi.spyOn(mockTicketService, 'create').mockReturnValue(of({ id: '1', ...testData }));
+
+    component.onSubmit();
+
+    expect(mockTicketService.create).toHaveBeenCalledWith(testData);
+  });  
+
+  it('should call goTickets when call onSubmit', () => {
+    vi.spyOn(component, 'goTickets');
+
+    component.onSubmit();
+
+    expect(component.goTickets).toHaveBeenCalled();
+  });
+
+  it('should navigate when call goTickets', () => {
+    component.goTickets();
+
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/tickets']);
+  });  
 });
--- frontend/src/app/features/tickets/pages/create-ticket-page/create-ticket-page.ts
+++ frontend/src/app/features/tickets/pages/create-ticket-page/create-ticket-page.ts
@@ -1,11 +1,39 @@
-import { Component } from '@angular/core';
+import { Component, inject } from '@angular/core';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { TicketApiService } from '../../data-access/ticket-api.service';
+import { Router } from '@angular/router';
+import { ITicketRequest } from '../../models/iticket-request.interface';
 
 @Component({
   selector: 'app-create-ticket-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './create-ticket-page.html',
   styleUrl: './create-ticket-page.css',
 })
 export class CreateTicketPage {
+  readonly ticketService = inject(TicketApiService)
+  readonly router = inject(Router)
+  readonly fb = inject(FormBuilder)
 
+  ticketForm = this.fb.group({
+    userId: [''],
+    title: [''],
+    description: ['']
+  })
+
+  onSubmit() {
+    if (this.ticketForm.valid) {
+      const newTicket = this.ticketForm.value as ITicketRequest
+
+      this.ticketService.create(newTicket).subscribe({
+      next: () => {
+        this.goTickets();
+      }
+    });
+    }
+  }
+
+  goTickets() {
+    this.router.navigate(['/tickets']);
+  }  
 }
