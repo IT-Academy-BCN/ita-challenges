5165c9d9abcb80c3bcd80b3beb4d7295f57b51ff
JordiCasas87 <Jordicasas87@gmail.com>
2026-05-11 12:23:51 +0200

    chore(ticketing): enable local cors for demo

--- backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/adapter/web/in/TicketController.java
+++ backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/adapter/web/in/TicketController.java
@@ -2,9 +2,11 @@ package com.ita.challenges.account.infrastructure.adapter.web.in;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
 @RestController
 @RequestMapping("/api/account/tickets")
+@CrossOrigin(origins = "http://localhost:4200")
 public class TicketController {
 
 }
