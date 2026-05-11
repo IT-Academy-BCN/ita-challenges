27fa4e8330f0d2712f35df26520e1966c37ba5fd
Juan Delgado <soy.juan.delgado@gmail.com>
2026-05-11 13:20:07 +0200

    feat: added create-ticket-page to routes

--- frontend/src/app/features/tickets/tickets.routes.ts
+++ frontend/src/app/features/tickets/tickets.routes.ts
@@ -1,9 +1,14 @@
 import { Routes } from '@angular/router';
 import { TicketsPage } from './pages/tickets-page.component';
+import { CreateTicketPage } from './pages/create-ticket-page/create-ticket-page';
 
 export const TICKETS_ROUTES: Routes = [
   {
     path: '',
     component: TicketsPage,
   },
+  {
+    path: 'create-ticket',
+    component: CreateTicketPage,
+  },  
 ];
