1606c880fb2b9c63f777e8f7260379a15f8ce1cd
Juan Delgado <soy.juan.delgado@gmail.com>
2026-05-11 12:47:20 +0200

    feat: added tickets service basic structure

---
+++ frontend/src/app/features/tickets/data-access/ticket-api.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import { ITicket } from '../models/iticket.interface';
+import { Observable, of } from 'rxjs';
+import { TICKETS_MOCK } from './tickets.mock';
+import { ITicketRequest } from '../models/iticket-request.interface';
+
+@Injectable({
+  providedIn: 'root',
+})
+export class TicketApiService {
+
+  create(ticket: ITicketRequest): Observable<ITicket> {
+    return of( { id: '1', ...ticket} );
+  }
+
+  loadAll(): Observable<ITicket[]> {
+    return of( TICKETS_MOCK );
+  }
+}
