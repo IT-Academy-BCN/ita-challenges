ce8a2f680227eb5ce2d8acc128aa5d602e9d5ca8
Spook242 <spook242@gmail.com>
2026-05-12 12:53:24 +0200

    feat: add ticket repository contract

--- backend/account-service/src/main/java/com/ita/challenges/account/domain/port/out/TicketRepository.java
+++ backend/account-service/src/main/java/com/ita/challenges/account/domain/port/out/TicketRepository.java
@@ -2,10 +2,6 @@ package com.ita.challenges.account.domain.port.out;
 
 import com.ita.challenges.account.domain.model.Ticket;
 
-import java.util.List;
-
 public interface TicketRepository {
-    List<Ticket> findAllByUserId(String userId);
-
-    Ticket updateTicket(Ticket ticket);
+    Ticket save(Ticket newTicket);
 }
