f3364122ac6da3a3c02319b527a22d8ef09f9144
Albert Medina <albertmecu@gmail.com>
2026-05-12 16:52:15 +0200

    fix: using RestClient instead of RestTemplate

--- backend/account-service/src/main/java/com/ita/challenges/account/AccountApplication.java
+++ backend/account-service/src/main/java/com/ita/challenges/account/AccountApplication.java
@@ -3,7 +3,7 @@ package com.ita.challenges.account;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
-import org.springframework.web.client.RestTemplate;
+import org.springframework.web.client.RestClient;
 
 @SpringBootApplication
 public class AccountApplication {
@@ -13,7 +13,7 @@ public class AccountApplication {
 	}
 
     @Bean
-    public RestTemplate restTemplate() {
-        return new RestTemplate();
+    public RestClient restClient() {
+        return RestClient.create();
     }
 }
