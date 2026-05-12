efcc76612e49574c4cbd5a0d12621788c582ef36
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-05-12 20:05:10 +0200

    feat(ticket): add base ticket list (#628)

--- frontend/src/app/features/tickets/tickets.routes.ts
+++ frontend/src/app/features/tickets/tickets.routes.ts
@@ -1,14 +1,9 @@
 import { Routes } from '@angular/router';
-import { TicketsPage } from './pages/tickets-page.component';
-import { CreateTicketPage } from './pages/create-ticket-page/create-ticket-page';
+import { TicketListPage } from './pages/ticket-list-page/ticket-list-page';
 
 export const TICKETS_ROUTES: Routes = [
   {
     path: '',
-    component: TicketsPage,
-  },
-  {
-    path: 'create-ticket',
-    component: CreateTicketPage,
-  },  
+    component: TicketListPage,
+  }
 ];
