8afe6ed09e52f34c23dceef638944d0ebfe0713e
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-05-12 20:19:38 +0200

    test(ticket): add ticket list tests (#628)

--- frontend/src/app/features/tickets/pages/ticket-list-page/ticket-list-page.spec.ts
+++ frontend/src/app/features/tickets/pages/ticket-list-page/ticket-list-page.spec.ts
@@ -1,6 +1,8 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { TicketListPage } from './ticket-list-page';
+import { TicketApiService } from '../../data-access/ticket-api.service';
+import { TICKETS_MOCK } from '../../models/tickets.mock';
 
 describe('TicketListPage', () => {
   let component: TicketListPage;
@@ -8,16 +10,35 @@ describe('TicketListPage', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      imports: [TicketListPage]
-    })
-    .compileComponents();
+      imports: [TicketListPage],
+      providers: [TicketApiService],
+    }).compileComponents();
 
     fixture = TestBed.createComponent(TicketListPage);
     component = fixture.componentInstance;
+
+    fixture.detectChanges();
     await fixture.whenStable();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should load tickets on init', () => {
+    expect(component.tickets()).toEqual(TICKETS_MOCK);
+  });
+
+  it('should render all tickets in template', () => {
+    const listItems = fixture.nativeElement.querySelectorAll('li');
+
+    expect(listItems.length).toBe(TICKETS_MOCK.length);
+  });
+
+  it('should render ticket title and description', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+
+    expect(compiled.textContent).toContain(TICKETS_MOCK[0].title);
+    expect(compiled.textContent).toContain(TICKETS_MOCK[0].description);
   });
 });
