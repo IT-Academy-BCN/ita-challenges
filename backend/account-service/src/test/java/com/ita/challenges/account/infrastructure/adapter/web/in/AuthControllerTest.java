cc2c6de078f511e1e7f2d17b1c514c3c105caec9
Albert Medina <albertmecu@gmail.com>
2026-05-12 18:09:12 +0200

    test: addd test for GitHub user not found

--- backend/account-service/src/test/java/com/ita/challenges/account/infrastructure/adapter/web/in/AuthControllerTest.java
+++ backend/account-service/src/test/java/com/ita/challenges/account/infrastructure/adapter/web/in/AuthControllerTest.java
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.client.AutoConfigureMockRestServiceServer;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.test.util.ReflectionTestUtils;
 import org.springframework.test.web.client.MockRestServiceServer;
@@ -23,6 +24,7 @@ import static org.mockito.ArgumentMatchers.*;
 import static org.mockito.Mockito.when;
 import static org.springframework.security.test.web.servlet.request.SecurityMockMvcRequestPostProcessors.csrf;
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.requestTo;
+import static org.springframework.test.web.client.response.MockRestResponseCreators.withStatus;
 import static org.springframework.test.web.client.response.MockRestResponseCreators.withSuccess;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
@@ -101,6 +103,22 @@ class AuthControllerTest {
                 .andExpect(status().isOk())
                 .andExpect(jsonPath("$.username").value("ID12345"))
                 .andExpect(jsonPath("$.role").value(Role.STUDENT.name()));
+    }
+
+    @Test
+
+    void create_shouldReturn404WhenGitHubUserNotFound() throws Exception {
+        this.mockServer.expect(requestTo("https://api.github.com/users/username12345"))
+                .andRespond(withStatus(HttpStatus.NOT_FOUND));
+
+        UserRequest request = new UserRequest("username12345", Role.GUEST);
+
+        mockMvc.perform(post("/api/account/auth/register")
+                        .with(oauth2Login()).with(csrf())
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(request))
+                )
+                .andExpect(status().isNotFound());
     }
 
     @Test
