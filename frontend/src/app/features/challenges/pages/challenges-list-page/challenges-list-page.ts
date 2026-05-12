da12111b55e907f4a415838cf81bd400704466df
nereame96 <nereame96@gmail.com>
2026-05-12 20:14:04 +0200

    fix(challenge): delete routerLink from challenge-list-page #545

--- frontend/src/app/features/challenges/pages/challenges-list-page/challenges-list-page.ts
+++ frontend/src/app/features/challenges/pages/challenges-list-page/challenges-list-page.ts
@@ -1,13 +1,12 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
 import { IChallenge } from '../../models/ichallenge.interface';
 import { ChallengeService } from '../../services/challenge.service';
-import { RouterLink } from '@angular/router';
 import { RoleSelectorComponent } from "../../components/role-selector/role-selector";
 import { CreateButtonComponent } from "../../components/buttons/create-button/create-button";
 
 @Component({
   selector: 'app-challenges-list-page',
-  imports: [RouterLink, RoleSelectorComponent, CreateButtonComponent],
+  imports: [RoleSelectorComponent, CreateButtonComponent],
   templateUrl: './challenges-list-page.html',
   styleUrl: './challenges-list-page.css',
 })
