dbfe1e9267787d6010250bbc7448729188934902
Albert Medina <albertmecu@gmail.com>
2026-05-12 15:18:58 +0200

    fix: changed the save method return type from void to User

--- backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/adapter/web/out/persistence/InMemoryUserRepository.java
+++ backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/adapter/web/out/persistence/InMemoryUserRepository.java
@@ -18,7 +18,8 @@ public class InMemoryUserRepository implements UserRepository {
     }
 
     @Override
-    public void save(User user) {
+    public User save(User user) {
         storage.put(user.userName(), user);
+        return user;
     }
 }
