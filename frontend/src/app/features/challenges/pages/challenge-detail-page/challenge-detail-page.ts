e2a5ad04c10892f042be986c7ffbe5989cf42ac6
nereame96 <nereame96@gmail.com>
2026-05-13 19:56:39 +0200

    feat(challenge): implement submission form and submit button logic #540

--- frontend/src/app/features/challenges/pages/challenge-detail-page/challenge-detail-page.ts
+++ frontend/src/app/features/challenges/pages/challenge-detail-page/challenge-detail-page.ts
@@ -2,16 +2,29 @@ import { Component, inject, signal } from '@angular/core';
 import { ChallengeService } from '../../services/challenge.service';
 import { ActivatedRoute } from '@angular/router';
 import { IChallenge } from '../../models/ichallenge.interface';
+import { ChallengeApiService } from '../../data-access/challenge-api.service';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { AuthService } from '../../../auth/data-access/auth-service';
+import { AuthUser } from '../../../auth/models/auth-user.model';
+import { IChallengeSubmission } from '../../models/ichallenge-submission.interface';
+
+interface AuthUserWithId extends AuthUser {
+  id: string;
+}
 
 @Component({
   selector: 'app-challenge-detail-page',
-  imports: [],
+  imports: [ReactiveFormsModule],
   templateUrl: './challenge-detail-page.html',
   styleUrl: './challenge-detail-page.css',
 })
+
 export class ChallengeDetailPage {
   private readonly challengesService = inject(ChallengeService);
   private readonly route = inject(ActivatedRoute);
+  private readonly challengeApiService = inject(ChallengeApiService)
+  private readonly fb = inject(FormBuilder)
+  private readonly authService = inject(AuthService)
 
   challenge = signal<IChallenge | undefined>(undefined);
 
@@ -22,4 +35,27 @@ export class ChallengeDetailPage {
       this.challenge.set(selectedChallenge);
     });
   }
+
+  codeSolutionForm = this.fb.group({
+      code: ['']
+    })
+
+    onSubmit() {
+      const currentChallenge = this.challenge();
+      const currentUser = this.authService.user() as AuthUserWithId;
+
+      if (this.codeSolutionForm.valid && currentChallenge?.id) {
+        const challengeSolution : IChallengeSubmission = {
+          challengeId: currentChallenge.id,
+          userId: currentUser?.id || '550e8400-e29b-41d4-a716-446655440000',
+          code: this.codeSolutionForm.value.code ?? ''
+          };
+
+        this.challengeApiService.postSolution(challengeSolution).subscribe({
+        next: () => {
+          alert('Solució al repte enviada!');
+        }
+      });
+      }
+    }
 }
