73337dc67fcd02b42f936c64166c45373296376b
Spook242 <spook242@gmail.com>
2026-05-13 14:55:47 +0200

    fix: use ConcurrentHashMap for thread safety and restore methods

--- backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/adapter/web/out/persistence/InMemoryTicketRepository.java
+++ backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/adapter/web/out/persistence/InMemoryTicketRepository.java
@@ -4,14 +4,14 @@ import com.ita.challenges.account.domain.model.Ticket;
 import com.ita.challenges.account.domain.port.out.TicketRepository;
 import org.springframework.stereotype.Repository;
 
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 import java.util.concurrent.ConcurrentHashMap;
 
 @Repository
 public class InMemoryTicketRepository implements TicketRepository {
-    private final Map<String, Ticket> database = new ConcurrentHashMap();
+
+    private final Map<String, Ticket> database = new ConcurrentHashMap<>();
 
     @Override
     public Ticket save(Ticket newTicket) {
