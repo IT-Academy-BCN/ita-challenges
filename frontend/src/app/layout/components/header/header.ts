024f404abbbec3dbf8f5787107047f71a013d549
Spook242 <spook242@gmail.com>
2026-05-13 17:49:13 +0200

    fix: restore accidentally deleted frontend files

--- frontend/src/app/layout/components/header/header.ts
+++ frontend/src/app/layout/components/header/header.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { LogoutButton } from '../../../shared/components/logout-button/logout-button';
 
 @Component({
   selector: 'app-header',
-  imports: [],
+  imports: [LogoutButton],
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
