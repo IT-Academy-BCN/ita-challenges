314820512ef4cc819cc9d349fe91c55571d6308e
Yvan Gabrieli <yvan.gabrielik@gmail.com>
2026-05-15 19:53:08 +0200

    feat(Submission): featured the enum with agreed with the team. might create conflict when merged.

---
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/submission/domain/valueobject/SubmissionStatus.java
@@ -0,0 +1,9 @@
+package com.itachallenges.challengeservice.submission.domain.valueobject;
+
+public enum SubmissionStatus {
+    NOT_STARTED,
+    IN_PROGRESS,
+    SUBMITTED,
+    APPROVED,
+    FAILED_REVIEW
+    }
