c908820116fc118e91669e10befc3bcfc8fd2947
Youssef El Gmoudi Aitouna <youssef.eg09@gmail.com>
2026-05-17 19:43:10 +0200

    Change API endpoint for submissions

--- backend/challenge-service/src/main/java/com/itachallenges/challengeservice/submission/infrastructure/adapter/in/web/controller/SubmissionController.java
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/submission/infrastructure/adapter/in/web/controller/SubmissionController.java
@@ -12,7 +12,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/v1/submissions")
+@RequestMapping("/api/challenge/submissions")
 public class SubmissionController {
 
     private final SubmissionRepository submissionRepository;
