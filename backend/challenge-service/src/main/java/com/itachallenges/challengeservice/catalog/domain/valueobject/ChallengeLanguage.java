b3cb97279a746445c41e95bb5fce79e14dfe857c
Spook242 <spook242@gmail.com>
2026-05-20 20:41:55 +0200

    feat: add ChallengeLanguage enum (closes #719)

---
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/catalog/domain/valueobject/ChallengeLanguage.java
@@ -0,0 +1,12 @@
+package com.itachallenges.challengeservice.catalog.domain.valueobject;
+
+
+    public enum Language {
+        JAVA,
+        SQL,
+        PYTHON,
+        JAVASCRIPT,
+        PHP,
+        TYPESCRIPT
+    }
+}
