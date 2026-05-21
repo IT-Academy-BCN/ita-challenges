75e62523bfa2ecbae776dfe00f7985faa7fd651e
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-05-21 10:21:04 +0200

    chore(challenges): update challenge mocks with full interface (#737)

--- frontend/src/app/features/challenges/models/challenges.mock.ts
+++ frontend/src/app/features/challenges/models/challenges.mock.ts
@@ -2,18 +2,27 @@ import { IChallenge } from './ichallenge.interface';
 
 export const CHALLENGES_MOCK: IChallenge[] = [
   {
-    id: "1",
-    title: "primer",
-    description: "descripció 1"
+    id: '1',
+    title: 'primer',
+    description: 'descripció 1',
+    solution: 'solució 1',
+    language: 'TYPESCRIPT',
+    difficulty: 'EASY',
   },
   {
-    id: "2",
-    title: "segon",
-    description: "descripció 2"
+    id: '2',
+    title: 'segon',
+    description: 'descripció 2',
+    solution: 'solució 2',
+    language: 'JAVA',
+    difficulty: 'MEDIUM',
   },
   {
-    id: "3",
-    title: "tercer",
-    description: "descripció 3"
+    id: '3',
+    title: 'tercer',
+    description: 'descripció 3',
+    solution: 'solució 3',
+    language: 'SQL',
+    difficulty: 'HARD',
   },
 ];
