fcab6d03753fbe446d14179c569ee168da73453d
nereame96 <nereame96@gmail.com>
2026-05-22 11:03:43 +0200

    feat(ticket): implement basic mocked html in ticket detail

--- frontend/src/app/features/tickets/pages/ticket-detail-page/ticket-detail-page.ts
+++ frontend/src/app/features/tickets/pages/ticket-detail-page/ticket-detail-page.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { ITicket } from '../../models/iticket.interface';
+import { TICKETS_MOCK } from '../../models/tickets.mock';
 
 @Component({
   selector: 'app-ticket-detail-page',
@@ -7,5 +9,7 @@ import { Component } from '@angular/core';
   styleUrl: './ticket-detail-page.css',
 })
 export class TicketDetailPage {
+
+  ticket: ITicket = TICKETS_MOCK[0];
 
 }
