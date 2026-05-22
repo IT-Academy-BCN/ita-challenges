5e7c50a1f2e9e894a7db466b22e95427a29e9a50
nereame96 <nereame96@gmail.com>
2026-05-22 13:13:02 +0200

    feat(ticketing): implement ticke-detail in routes and testing #779

--- frontend/src/app/features/tickets/pages/ticket-detail-page/ticket-detail-page.spec.ts
+++ frontend/src/app/features/tickets/pages/ticket-detail-page/ticket-detail-page.spec.ts
@@ -1,6 +1,6 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { TicketDetailPage } from './ticket-detail-page';
+import { TICKETS_MOCK } from '../../models/tickets.mock';
 
 describe('TicketDetailPage', () => {
   let component: TicketDetailPage;
@@ -19,5 +19,15 @@ describe('TicketDetailPage', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should render ticket details in the HTML', () => {
+    const compiled = fixture.nativeElement as HTMLElement;
+    const titleElement = compiled.querySelector('h1');
+    const paragraphs = compiled.querySelectorAll('p');
+
+    expect(titleElement?.textContent).toContain(TICKETS_MOCK[0].title);
+    expect(paragraphs[0]?.textContent).toContain(TICKETS_MOCK[0].userId);
+    expect(paragraphs[1]?.textContent).toContain(TICKETS_MOCK[0].description);
   });
 });
