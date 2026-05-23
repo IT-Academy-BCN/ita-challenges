d351146bb9e12bbce2be95b992d5d988a1655e20
Rafael di Candia <141920523+Rafadicandia@users.noreply.github.com>
2026-05-23 19:01:05 +0200

    feat(FinalizeSubmissionUseCaseHandler): Implement the execute(FinalizeSubmissionCommand command) method inside FinalizeSubmissionUseCaseHandler.                                         Orchestrate the infrastructure call to the Submission repository using the CreateSubmitted method.

--- backend/challenge-service/src/main/java/com/itachallenges/challengeservice/submission/application/usecase/FinalizeSubmissionUseCaseHandler.java
+++ backend/challenge-service/src/main/java/com/itachallenges/challengeservice/submission/application/usecase/FinalizeSubmissionUseCaseHandler.java
@@ -1,19 +1,40 @@
 package com.itachallenges.challengeservice.submission.application.usecase;
 
+import com.itachallenges.challengeservice.catalog.domain.valueobject.ChallengeId;
+import com.itachallenges.challengeservice.shared.domain.valueobject.UserId;
 import com.itachallenges.challengeservice.submission.application.dto.FinalizeSubmissionCommand;
+import com.itachallenges.challengeservice.submission.domain.Submission;
 import com.itachallenges.challengeservice.submission.domain.port.in.FinalizeSubmissionUseCase;
+import com.itachallenges.challengeservice.submission.domain.port.out.SubmissionRepository;
+import com.itachallenges.challengeservice.submission.domain.valueobject.SubmissionId;
+import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Service;
 
+import java.util.Optional;
+import java.util.UUID;
+
 @Service
+@AllArgsConstructor
 public class FinalizeSubmissionUseCaseHandler implements FinalizeSubmissionUseCase {
-    // TODO: inject SubmissionRepository and any other dependencies
+    private  final SubmissionRepository repository;
 
     @Override
     public void execute(FinalizeSubmissionCommand command) {
-        // TODO: check no FINAL submission exists for this user/challenge
-        // TODO: load or create submission
-        // TODO: call submission.finalize()
-        // TODO: save submission
-        // TODO: publish domain events
+        ChallengeId challengeId = new ChallengeId(UUID.fromString(command.challengeId()));
+        UserId userId = new UserId(UUID.fromString(command.userId()));
+
+        Optional<Submission> existingSubmission = repository.findByUserAndChallenge(userId, challengeId);
+        if (existingSubmission.isPresent()) {
+            throw new RuntimeException("Challenge was submited before by User:" + command.userId());
+        }
+
+        Submission submission = Submission.createSubmitted(
+                SubmissionId.generate(),
+                challengeId,
+                userId,
+                command.code()
+        );
+
+        repository.save(submission);
     }
 }
