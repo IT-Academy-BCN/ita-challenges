73de9602171b70b868cc4d4615ad9490134f5724
Urian1983 <urianr@gmail.com>
2026-05-25 13:07:41 +0200

    Changing the name of the Record/DTO to adjust it to the controller

--- backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/adapter/web/in/dto/TicketPatchResponse.java
+++ backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/adapter/web/in/dto/TicketPatchResponse.java
@@ -2,13 +2,6 @@ package com.ita.challenges.account.infrastructure.adapter.web.in.dto;
 
 import com.ita.challenges.account.domain.model.TicketStatus;
 
-import java.time.Instant;
-
-public record TicketPatchResponse(String id,
-                                  String userId,
-                                  String title,
-                                  String comment,
-                                  TicketStatus status,
-                                  Instant createdAt,
-                                  Instant updatedAt) {
+public record TicketPatchResponse(String id, String userId, String title, String description, TicketStatus ticketStatus, String ticketComment
+                                  ) {
 }
