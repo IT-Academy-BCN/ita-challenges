2d50afb05945e82cfd89f48533f091302bac5758
Urian1983 <urianr@gmail.com>
2026-05-25 12:12:23 +0200

    Changing the name of the Record/DTO to adjust it to the controller

--- backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/adapter/web/in/dto/TicketCommentAndStatusResponse.java
+++
@@ -1,6 +0,0 @@
-package com.ita.challenges.account.infrastructure.adapter.web.in.dto;
-
-import com.ita.challenges.account.domain.model.TicketStatus;
-
-public record TicketCommentAndStatusResponse(String comment, TicketStatus status) {
-}
