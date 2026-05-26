d2362ff1532a5ec948e2145b8709d50ba8658d40
Jordi Miravet <jordimiravet.dev@gmail.com>
2026-05-26 20:06:32 +0200

    feat(challenges): add delete button to challenges list page (#743)

--- frontend/src/app/features/challenges/pages/challenges-list-page/challenges-list-page.ts
+++ frontend/src/app/features/challenges/pages/challenges-list-page/challenges-list-page.ts
@@ -1,13 +1,20 @@
 import { Component, inject, OnInit, signal } from '@angular/core';
+import { RouterLink } from "@angular/router";
+
 import { IChallenge } from '../../models/ichallenge.interface';
 import { ChallengeService } from '../../services/challenge.service';
 import { RoleSelectorComponent } from "../../components/role-selector/role-selector";
 import { CreateButtonComponent } from '../../components/buttons/create-button/create-button';
-import { RouterLink } from "@angular/router";
+import { DeleteButtonComponent } from "../../components/buttons/delete-button/delete-button";
 
 @Component({
   selector: 'app-challenges-list-page',
-  imports: [CreateButtonComponent, RoleSelectorComponent, RouterLink],
+  imports: [
+    RouterLink, 
+    RoleSelectorComponent, 
+    CreateButtonComponent, 
+    DeleteButtonComponent
+  ],
   standalone: true,
   templateUrl: './challenges-list-page.html',
   styleUrl: './challenges-list-page.css',
@@ -34,4 +41,15 @@ export class ChallengesListPage implements OnInit {
   onRoleChange(value: boolean): void {
     this.isMentor.set(value);
   }
+
+  handleDelete(id: string): void {
+    this.challengesService.delete(id).subscribe({
+      next: () => {
+        this.challenges.update((current) =>
+          current.filter((challenge) => challenge.id !== id)
+        );
+      }
+    });
+  }
+
 }
