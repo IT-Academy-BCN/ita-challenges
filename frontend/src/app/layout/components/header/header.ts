845904387549e049f629308cfe215bacf1960e13
Spook242 <spook242@gmail.com>
2026-06-07 13:23:46 +0200

    fix: restore ngOnInit and fetchUser to sync with backend

--- frontend/src/app/layout/components/header/header.ts
+++ frontend/src/app/layout/components/header/header.ts
@@ -8,10 +8,17 @@ import { AuthService } from '../../../features/auth/data-access/auth-service';
   templateUrl: './header.html',
   styleUrl: './header.css',
 })
-export class Header {
+export class Header implements OnInit {
   private readonly authService = inject(AuthService);
 
   public user = computed(() => this.authService.user());
 
+  ngOnInit(): void {
+      if (this.authService.getUser()) return;
+
+      this.authService.fetchUser().subscribe({
+        next: (user) => this.authService.setUser(user),
+      });
+    }
 }
 
