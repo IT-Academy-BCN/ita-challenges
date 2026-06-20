8e8648e0f9ffe3c46d7cf19fe0fd555442d9211c
Urian1983 <urianr@gmail.com>
2026-06-20 20:07:51 +0200

    adding the tests for login button

---
+++ frontend/src/app/shared/components/login-button/login-button.spec.ts
@@ -0,0 +1,52 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { LoginButton } from './login-button';
+import { Router } from @angular/router";
+
+describe('LoginButton', () => {
+  let component: LoginButton;
+  let fixture: ComponentFixture<LoginButton>;
+  let routerMock: {
+    navigate: ReturnType<typeof vi.fn>;
+  };
+
+  beforeEach(async () => {
+    routerMock = {
+      navigate: vi.fn(),
+    };
+
+    await TestBed.configureTestingModule({
+      imports: [LoginButton],
+      providers: [
+        { provide: Router, useValue: routerMock },
+        ],
+    }).compileComponents();
+
+    fixture = TestBed.createComponent(LoginButton);
+    component = fixture.componentInstance;
+
+    await fixture.whenStable();
+
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+
+  it('should call login method when button is clicked', () => {
+    const spy = vi.spyOn(component, 'login');
+
+    const button: HTMLButtonElement =
+      fixture.nativeElement.querySelector('button');
+
+    button.click();
+
+    expect(spy).toHaveBeenCalled();
+  });
+
+  it('should navigate to /auth when login is called', () => {
+    component.login();
+
+    expect(routerMock.navigate).toHaveBeenCalledWith(['/auth']);
+  });
+});
