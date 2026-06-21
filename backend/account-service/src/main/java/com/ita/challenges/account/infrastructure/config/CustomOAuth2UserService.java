1e81b1f2d331314b282896ce09ed218d7df133b5
Spook242 <spook242@gmail.com>
2026-06-21 21:14:46 +0200

    feat(auth): assign 'Student' role automatically on OAuth2 login and test

---
+++ backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/config/CustomOAuth2UserService.java
@@ -0,0 +1,37 @@
+package com.ita.challenges.account.infrastructure.config;
+
+import com.ita.challenges.account.domain.model.Role;
+import com.ita.challenges.account.domain.model.User;
+import com.ita.challenges.account.domain.port.out.UserRepository;
+import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
+import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
+import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.OAuth2User;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CustomOAuth2UserService extends DefaultOAuth2UserService {
+
+    private final UserRepository userRepository;
+
+    public CustomOAuth2UserService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
+        OAuth2User oAuth2User = super.loadUser(userRequest);
+        processOAuth2User(oAuth2User);
+
+        return oAuth2User;
+    }
+
+    public void processOAuth2User(OAuth2User oAuth2User) {
+        String login = oAuth2User.getAttribute("login");
+
+        if (login != null && userRepository.findByUsername(login).isEmpty()) {
+            User newUser = new User(login, Role.STUDENT);
+            userRepository.save(newUser);
+        }
+    }
+}
