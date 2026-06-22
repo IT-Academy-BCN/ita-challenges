1ee9f0ff808fa450b921c43139ad6d07e702f507
Spook242 <spook242@gmail.com>
2026-06-22 19:57:18 +0200

    feat: refactor CustomOAuth2UserService for PR #1027

--- backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/config/CustomOAuth2UserService.java
+++ backend/account-service/src/main/java/com/ita/challenges/account/infrastructure/config/CustomOAuth2UserService.java
@@ -3,11 +3,16 @@ package com.ita.challenges.account.infrastructure.config;
 import com.ita.challenges.account.domain.model.Role;
 import com.ita.challenges.account.domain.model.User;
 import com.ita.challenges.account.domain.port.out.UserRepository;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
 import org.springframework.security.oauth2.client.userinfo.DefaultOAuth2UserService;
 import org.springframework.security.oauth2.client.userinfo.OAuth2UserRequest;
 import org.springframework.security.oauth2.core.OAuth2AuthenticationException;
+import org.springframework.security.oauth2.core.user.DefaultOAuth2User;
 import org.springframework.security.oauth2.core.user.OAuth2User;
 import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+import java.util.List;
 
 @Service
 public class CustomOAuth2UserService extends DefaultOAuth2UserService {
@@ -21,9 +26,18 @@ public class CustomOAuth2UserService extends DefaultOAuth2UserService {
     @Override
     public OAuth2User loadUser(OAuth2UserRequest userRequest) throws OAuth2AuthenticationException {
         OAuth2User oAuth2User = super.loadUser(userRequest);
+
         processOAuth2User(oAuth2User);
 
-        return oAuth2User;
+        List<SimpleGrantedAuthority> authorities = Collections.singletonList(
+                new SimpleGrantedAuthority("ROLE_STUDENT")
+        );
+
+        return new DefaultOAuth2User(
+                authorities,
+                oAuth2User.getAttributes(),
+                "login"
+        );
     }
 
     public void processOAuth2User(OAuth2User oAuth2User) {
